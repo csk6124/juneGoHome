87717be4ecb0356f6d5db746e06c11d4100e5abc
csk6124 <csk6124@gmail.com>
2015-07-24 18:37:45 +0900

    update

--- web/server.go
+++ web/server.go
@@ -1,3 +1,14 @@
+/*
+ web package example
+ 아래 소스를 참조하여 테스트 프로그램 작성
+ source : http://www.alexedwards.net/blog/a-recap-of-request-handling
+
+ ServeMuxes: http request router(or muxtiplexor)
+ Handlers: writing response headers and bodies
+
+ ServeHTTP(http.ResponseWriter, *http.Request)
+
+*/
 package web
 
 import (
@@ -6,19 +17,22 @@ import (
 	"time"
 )
 
-type timeHandler struct {
-  format string
-}
 
-func (th *timeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  tm := time.Now().Format(th.format)
-  w.Write([]byte("The time is: " + tm))
+func timeHandler(format string) http.Handler {
+	// closure function
+  fn := func(w http.ResponseWriter, r *http.Request) {
+    tm := time.Now().Format(format)
+    w.Write([]byte("The time is: " + tm))
+  }
+  return http.HandlerFunc(fn)
 }
 
 func main() {
 	mux := http.NewServeMux()
 
-	th := &timeHandler{format: time.RFC1123}
+	var format string = time.RFC1123
+	th := timeHandler(format)
+
  	mux.Handle("/", th)
 
  	log.Println("Listening....")
